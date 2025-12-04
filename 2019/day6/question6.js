313ab993e651e64b3ca2e67cf923280b722b17c4
Santo K M <santokm@Santos-MacBook-Pro.local>
2025-12-04 23:16:55 +0530

    Day 6 made the orbits into a big object

--- 2019/day6/question6.js
+++ 2019/day6/question6.js
@@ -1,6 +1,31 @@
 const spaceMap = Deno.readTextFileSync("./smallData.txt");
-const connection = spaceMap.split("\n").map((x) => {
- const [planet, satellite] = x.split(")")
- return {planet, satellite}
-})
-console.log(connection)
+
+const makePlantConnections = (spaceMap) =>
+  spaceMap.split("\n").reduce((res, x) => {
+    const [planet, satellite] = x.split(")");
+    res[satellite] = planet ;
+    return res;
+  },{});
+
+// const makePlantConnections = (spaceMap) =>
+//   spaceMap.split("\n").map((x) => {
+//     const [planet, satellite] = x.split(")");
+//     return { [satellite]: planet };
+//   });
+
+// const countOrbits = (curretPlanet, plantConnections) => {
+//   let orbits = 0;
+//   let planet = curretPlanet;
+//   while (planet !== "COM") {
+//     planet = plantConnections[planet];
+//     orbits++;
+//   }
+// }
+
+const main = (spaceMap) => {
+  const plantConnections = makePlantConnections(spaceMap);
+  console.log(spaceMap.split("\n"))
+  console.log(plantConnections)
+};
+
+main(spaceMap);
