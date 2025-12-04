6101f1f1b428935cb57143f77957f33c5d6685a1
Santo K M <santokm@Santos-MacBook-Pro.local>
2025-12-04 23:59:20 +0530

    day 6 part 2 done

--- 2019/day6/question6.js
+++ 2019/day6/question6.js
@@ -4,27 +4,49 @@ const spaceMap = Deno.readTextFileSync("./bigData.txt");
 const makePlantConnections = (spaceMap) =>
   spaceMap.split("\n").reduce((res, x) => {
     const [planet, satellite] = x.split(")");
-    res[satellite] = planet ;
+    res[satellite] = planet;
     return res;
-  },{});
+  }, {});
 
-const countOrbits = (curretPlanet, plantConnections) => {
+const countOrbits = (curretPlanet, plantConnections, target = "COM") => {
   let orbits = 0;
   let planet = curretPlanet;
-  while (planet !== "COM") {
+  while (planet !== target) {
     planet = plantConnections[planet];
     orbits++;
   }
-  return orbits
-}
+  return orbits;
+};
+
+const plotPath = (currentPos, plantConnections) => {
+  const path = [];
+  let planet = currentPos;
+  while (planet !== "COM") {
+    planet = plantConnections[planet];
+    path.push(planet);
+  }
+  return path;
+};
+
+const findPathmeet = (youPath, sanPath) => {
+  for (let index = 0; index < youPath.length; index++) {
+    if (sanPath.includes(youPath[index])) return youPath[index];
+  }
+};
 
 const main = (spaceMap) => {
   const plantConnections = makePlantConnections(spaceMap);
   let totalOrbitCount = 0;
   for (const planet in plantConnections) {
-    totalOrbitCount += countOrbits(planet,plantConnections)
-  };
-  console.log(totalOrbitCount)
+    totalOrbitCount += countOrbits(planet, plantConnections);
+  }
+
+  const youPath = plotPath("YOU", plantConnections);
+  const sanPath = plotPath("SAN", plantConnections);
+  const pathMeet = findPathmeet(youPath, sanPath);
+
+  const orbitChanges = youPath.indexOf(pathMeet) + sanPath.indexOf(pathMeet);
+  return { totalOrbitCount, orbitChanges };
 };
 
-main(spaceMap);
+console.log(main(spaceMap));
